40d38134fb458c09da0e1ad13d7e3721f6262fd9
Rekuiem84 <morinijoshua@gmail.com>
2023-07-15 01:04:13 +0200

    end of the project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# key-combo-navbar
+# Key Combo Navbar
+
+Seventh project of [this challenge](https://github.com/Rekuiem84/personal-challenge).
+
+### What this project contains :
+
+- 4 hidden navbars (with one already kinda styled) that appear when one, or more keys are pressed
+
+### What remarkable/unusual properties are used ?
+
+CSS :
+
+- Native CSS **nesting**
+
+JS :
+
+- **keydown** event to detect when and what a key is pressed
--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-let navbar = document.querySelector(".navbar");
-let isNavOpened = false;
+let navbar1 = document.querySelector(".nav1");
+let isNav1Opened = false;
+let navbar2 = document.querySelector(".nav2");
+let isNav2Opened = false;
+let navbar3 = document.querySelector(".nav3");
+let isNav3Opened = false;
+let navbar4 = document.querySelector(".nav4");
+let isNav4Opened = false;
 
 document.onkeydown = keydown;
+
 function keydown(event) {
-	console.log("event : ", event.keyCode, event);
 	if ((event.ctrlKey || event.metaKey) && event.key == "m") {
-		!isNavOpened
-			? (navbar.style.translate = "0")
-			: (navbar.style.translate = "-100%");
-		isNavOpened = !isNavOpened;
-		console.log(
-			"ctrl :",
-			event.ctrlKey,
-			", cmd :",
-			event.metaKey,
-			", m :",
-			event.key == "m"
-		);
+		toggleNav("left");
+	}
+	if (event.key == "n") {
+		toggleNav("right");
+	}
+	if (event.key == "t") {
+		toggleNav("top");
+	}
+	if ((event.ctrlKey || event.metaKey) && event.key == "s") {
+		toggleNav("bottom");
 	}
 }
-//   document.onkeydown = function(e) {
-//         if (e.altKey && (e.keyCode === 67||e.keyCode === 86)) {//Alt+c, Alt+v will also be disabled sadly.
-//             alert('not allowed');
-//         }
-//         return false;
-// };​
+
+function toggleNav(position) {
+	if (position == "left") {
+		!isNav1Opened
+			? (navbar1.style.translate = "0")
+			: (navbar1.style.translate = "100%");
+		isNav1Opened = !isNav1Opened;
+	}
+	if (position == "right") {
+		!isNav2Opened
+			? (navbar2.style.translate = "0")
+			: (navbar2.style.translate = "-100%");
+		isNav2Opened = !isNav2Opened;
+	}
+	if (position == "top") {
+		!isNav3Opened
+			? (navbar3.style.translate = "0")
+			: (navbar3.style.translate = "0 -100%");
+		isNav3Opened = !isNav3Opened;
+	}
+	if (position == "bottom") {
+		!isNav4Opened
+			? (navbar4.style.translate = "0")
+			: (navbar4.style.translate = "0 100%");
+		isNav4Opened = !isNav4Opened;
+	}
+}
